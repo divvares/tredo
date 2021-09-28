89f511ffd1f82905533ee63d74419717b80e2d23
divvares <sultanelaman@gmail.com>
2021-09-28 16:39:52 +0600

    initial commit

---
+++ welcomeDrink/index.js
@@ -0,0 +1,69 @@
+Vue.component('todo-item', {
+  // Компонент todo-item теперь принимает
+  // "prop", то есть входной параметр.
+  // Имя входного параметра todo.
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'Вы загрузили эту страницу: ' + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'learn js' },
+      { text: 'learn vuejs' },
+      { text: 'create something cool' }
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'hello, vue js!'
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'hello, vue'
+  }
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Овощи' },
+      { id: 1, text: 'Сыр' },
+      { id: 2, text: 'Что там ещё люди едят?' }
+    ]
+  }
+})
